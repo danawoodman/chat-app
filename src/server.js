1bca5f3af3fd9be2d51cd6a0c270aff9a487fe80
Zachary Taylor <zachwtaylor@gmail.com>
2015-09-29 19:44:01 -0400

    Added tutorial CSS for form, installed socket.io with npm

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const chalk = require('chalk')
 const express = require('express')
-//const rooms = require('./routes/room')
 
 const app = express()
+const http = require('http')
+const io = require('socket.io')(http)
+//const rooms = require('./routes/room')
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
@@ -15,9 +17,14 @@ const PORT = process.env.PORT || 3001
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+io.on('connection', (socket) => {
+	console.log('a user connected')
+})
+
 const server = app.listen(PORT, () => {
   const host = server.address().address
   const port = server.address().port
 
-  console.info(chalk.green(`Example app listening at`), chalk.blue(`http://${host}:${port}`))
+  console.info(chalk.green(`Chat app listening at`), chalk.blue(`http://${host}:${port}`))
 })
